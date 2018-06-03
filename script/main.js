57ab98462d3368a842d84cbe46b1a1c091ba73c8
Norbert Miko <m.norbert96@gmail.com>
2018-06-03 15:02:24 +0200

    header scroll color change added

--- script/main.js
+++ script/main.js
@@ -1,2 +1,10 @@
 //Header background controller:
 var header = document.querySelector('header');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 600) {
+        header.classList.add('active');
+
+    } else {
+        header.classList.remove('active');
+    }
+});
